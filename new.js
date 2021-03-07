f235117303b80e090e8d90284cccb33f0ec42a65
Neha Tomar <tneha5161@gmail.com>
2021-03-07 19:53:30 +0530

    form

---
+++ new.js
@@ -0,0 +1,6 @@
+function submit(){
+  var form= document.getElementById("name").value;
+  var form= document.getElementById("number").value;
+  var form= document.getElementById("text").value;
+  } 
+  console.log("name,number,text")
